c691beeefd9619724bb7e241f01644ca395f3135
Christian Lambert <christianmarklambert@gmail.com>
2020-05-30 14:54:55 -0500

    Basic PoC working

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Card from "./Card.svelte";
+
+  let modifiers = [
+    "Double Damage - Bless",
+    "Double Damage",
+    "Minus 1",
+    "Minus 2",
+    "No Damage - Curse",
+    "Plus 1",
+    "Plus 2",
+    "Neutral"
+  ];
+
+  let counter = 0;
+</script>
+
+<main>
+  <div on:click={() => (counter = (counter + 1) % modifiers.length)}>
+    <Card modifier={modifiers[counter]} />
+  </div>
+</main>
---
+++ src/Card.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let character;
+  export let modifier;
+  export let status;
+  export let element;
+  export let roll;
+</script>
+
+<style>
+  div {
+    position: relative;
+    top: 0;
+    left: 0;
+  }
+
+  #blank {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  #modifier {
+    position: relative;
+    left: 0px;
+    top: 0px;
+  }
+</style>
+
+<div>
+  <img id="blank" src="images/blank.jpg" alt="modifier card" />
+  <img id="modifier" src={`images/${modifier}.png`} alt="" />
+  {#if status}
+    <img id="status" src={`images/${status}.png`} alt={status} />
+  {/if}
+  {#if character}
+    <img id="character" src={`images/${character}.png`} alt={character} />
+  {/if}
+  {#if element}
+    <img id="element" src={`images/${element}.png`} alt={element} />
+  {/if}
+  {#if roll}
+    <img id="roll" src={`images/roll.png`} alt={roll} />
+  {/if}
+
+</div>
